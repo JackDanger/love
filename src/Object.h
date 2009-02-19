aaed325bb037faa4fb4bc6041589a913b57d83dd
prerude <prerude@3494dbca-881a-0410-bd34-8ecbaf855390>
2009-02-19 13:47:24 +0000

    Updated all headers to include license information.
    git-svn-id: https://love.svn.sf.net/svnroot/love/trunk@685 3494dbca-881a-0410-bd34-8ecbaf855390

--- src/Object.h
+++ src/Object.h
@@ -1,21 +1,26 @@
 /**
-* LOVE: Free 2D Game Engine
-* Website: http://love2d.org
-* Licence: ZLIB/libpng
-* Copyright (c) 2006-2009 LOVE Development Team
+* LOVE -- Free 2D Game Engine
+* Version $(DOC_VERSION), $(DOC_DATE)
 * 
-* Superclass for all object that should be able to 
-* cross the Lua/C border (this pertains to most objects).
-*
-* This class is an alternative to using smart pointers;
-* it contains retain/release methods, and will delete
-* itself with the reference count hits zero.
+* Copyright (c) 2006-$(DOC_YEAR) LOVE Development Team
 * 
-* The wrapper code assumes that all userdata inherits from
-* this class.
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
 * 
-* @author Anders Ruud
-* @date 2008-10-30
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 
+* 1. The origin of this software must not be misrepresented; you must not
+*    claim that you wrote the original software. If you use this software
+*    in a product, an acknowledgment in the product documentation would be
+*    appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+*    misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+* 
+* -- LOVE Development Team, http://love2d.org
 **/
 
 #ifndef LOVE_OBJECT_H
@@ -23,6 +28,17 @@
 
 namespace love
 {
+	/**
+	* Superclass for all object that should be able to 
+	* cross the Lua/C border (this pertains to most objects).
+	*
+	* This class is an alternative to using smart pointers;
+	* it contains retain/release methods, and will delete
+	* itself with the reference count hits zero.
+	* 
+	* The wrapper code assumes that all userdata inherits from
+	* this class.
+	**/
 	class Object
 	{
 	private:
